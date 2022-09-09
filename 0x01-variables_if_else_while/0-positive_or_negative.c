13dfe64ade3c459eb2c3a4f7dd89dee0404de254
mustapha-ma <mustaphamubarak8@gmail.com>
2022-09-09 14:01:36 -0700

    task o

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,6 @@
 #include <time.h>
 
 /**
- *
  * main - Entry point
  *  Return: Always 0 (Success)
  */
@@ -20,8 +19,7 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	} else
-	{	
-		printf("%d is negative\n", n);
+	{	printf("%d is negative\n", n);
 	}
 	return (0);
 }
