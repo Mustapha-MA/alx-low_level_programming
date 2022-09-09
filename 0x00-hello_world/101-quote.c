666544953a5d9d81b2fee4a72531affd745153f1
MAVENLEAD <mustaphamubarak8@gmail.com>
2022-09-09 09:18:09 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *
  * Return: Always 0 (Success)
  */
 in main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
 	return (1);
 }                                                                                                                                 
