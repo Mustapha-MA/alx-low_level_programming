71a1d9699ff596b9c836c0990323f4840fc3eb67
mustapha-ma <mustaphamubarak8@gmail.com>
2022-09-11 22:51:58 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,51 +1,34 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
 
-		int d = 48;
+	int d = 48;
 
-			char c = 'a';
-
-
-
-				while (d < 58)
-
-						{
-
-									putchar(d);
-
-											d++;
-
-												}
-
-					while (c <= 'f')
-
-							{
-
-										putchar(c);
-
-												c++;
-
-													}
-
-						putchar('\n');
+	char c = 'a';
 
 
+	while (d < 58)
+	{
+		putchar(d);
+		d++;
+	}
 
-							return (0);
+	while (c <= 'f')
 
+	{
+
+		putchar(c);
+		c++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
 }
