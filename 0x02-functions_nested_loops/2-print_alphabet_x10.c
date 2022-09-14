1896e483d4ce872aa7eaf29e44a854e75534f8d0
mustapha-ma <mustaphamubarak8@gmail.com>
2022-09-14 07:06:01 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,40 @@
 #include "main.h"
 
+
+
 /**
+
  * print_alphabet_x10 - prints the alphabet 10 times
+
  *
+
  * Return: Always 0
+
  */
+
+
 
 void print_alphabet_x10(void)
 
+
+
 	int i, j;
 
+
+
 	for (i = 0; i <= 9; i++)
+
 	{
+
 		for (j = 97; j <= 122; j++)
+
 		{
+
 			_putchar(j);
+
 		}
+
 		_putchar('\n);
+
 	}
-}
+
