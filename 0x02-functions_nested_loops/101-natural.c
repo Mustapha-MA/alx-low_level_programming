62c41d4c98a3bb497d6fd4eaae556a015f4e7e8d
MAVENLEAD <mustaphamubarak8@gmail.com>
2022-09-14 18:40:47 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+#include <math.h>
+
 /**
- * print_number - a function that prints an integer
- * @n: input
- * Return: inputted integer
+ * main - main block
+ * Description: Print the sum of all multiples of 3 or 5 below 1024.
+ * Return: 0
  */
-void print_number(int n)
+
+int main(void)
 {
+	int i = 1;
+	int total = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+			total += i;
+		else if (i % 5 == 0)
+			total += i;
+
+		i++;
+	}
+	printf("%d\n", total);
 
 	return (0);
 }
